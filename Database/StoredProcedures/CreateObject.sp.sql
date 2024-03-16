008d6518f1b05243c51c1c4d695a0d5afaa3a5d2
MartinTande <mrtande95@gmail.com>
2024-03-16 20:11:56 +0100

    Add create object SP

---
+++ Database/StoredProcedures/CreateObject.sp.sql
@@ -0,0 +1,34 @@
+IF EXISTS (SELECT name
+	FROM sysobjects
+	WHERE name = 'CreateObject'
+	AND type = 'P')
+DROP PROCEDURE CreateObject
+GO
+
+CREATE PROCEDURE CreateObject
+	@ObjectName varchar(50),
+	@ObjectType varchar(50),
+	@Hierarchy1 varchar(50),
+	@Hierarchy2 varchar(50),
+	@EasGroup varchar(50),
+	@Otd varchar(50)
+
+AS
+
+DECLARE
+	@ObjectTypeId int,
+	@Hierarchy1Id varchar(50),
+	@Hierarchy2Id varchar(50),
+	@EasGroupId varchar(50),
+	@OtdId varchar(50)
+
+SELECT @ObjectTypeId=ObjectTypeId FROM OBJECT_TYPE WHERE ObjectType=@ObjectType
+SELECT @Hierarchy1Id=Hierarchy1Id FROM HIERARCHY_1 WHERE Hierarchy1=@Hierarchy1
+SELECT @Hierarchy2Id=Hierarchy2Id FROM HIERARCHY_2 WHERE Hierarchy2=@Hierarchy2
+SELECT @EasGroupId=EasGroupId FROM EAS_GROUP WHERE EasGroup=@EasGroup
+SELECT @OtdId=OtdId FROM OTD WHERE Otd=@Otd
+
+INSERT INTO TAG_OBJECT (ObjectName, ObjectTypeId, Hierarchy1Id, Hierarchy2Id, EasGroupId, OtdId) 
+VALUES (@ObjectName, @ObjectTypeId, @Hierarchy1Id, @Hierarchy2Id, @EasGroupId, @OtdId)
+
+GO
