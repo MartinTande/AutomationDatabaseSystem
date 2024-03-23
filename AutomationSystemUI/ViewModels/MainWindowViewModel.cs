0601c3f99f58bd9445430df971684d8cfe780dbc
MartinTande <mrtande95@gmail.com>
2024-03-23 18:42:10 +0100

    Add Mircoburn package, create viewmodelbase

---
+++ AutomationSystemUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AutomationSystemUI.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationSystemUI.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
