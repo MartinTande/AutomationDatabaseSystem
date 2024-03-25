4bcc74208b4ecf8ae63f0ce104a3fe0b5a5c3550
MartinTande <mrtande95@gmail.com>
2024-03-25 13:11:33 +0100

    Add GetObjectById SP

---
+++ Database/StoredProcedures/GetObjectById.sp.sql
@@ -0,0 +1,19 @@
+-- Check if Stored Procedure exists and deletes it if it does
+IF EXISTS (SELECT name
+	FROM sysobjects
+	WHERE name = 'GetObjectById'
+	AND type = 'P')
+DROP PROCEDURE GetObjectById
+GO
+
+CREATE PROCEDURE GetObjectById
+	@Id int
+AS
+
+BEGIN
+
+SELECT *
+FROM GetTagObjectData
+WHERE ObjectId=@Id;
+
+END;
