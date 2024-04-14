a371a89accdd0a3cf4ad2241cc1b3062e63003c9
MartinTande <mrtande95@gmail.com>
2024-04-14 21:13:12 +0200

    Update SP

--- Database/StoredProcedures/Category/GetCategoryNames.sp.sql
+++ Database/StoredProcedures/Category/GetCategoryNames.sp.sql
@@ -1,19 +1,19 @@
 -- Check if View exists and deletes it if yes
 IF EXISTS (SELECT name
 	FROM sysobjects
-	WHERE name = 'GetCategory'
+	WHERE name = 'GetCategoryNames'
 	AND type = 'P')
-DROP PROCEDURE GetCategory
+DROP PROCEDURE GetCategoryNames
 GO
 
-CREATE PROCEDURE GetCategory
+CREATE PROCEDURE GetCategoryNames
     @TableName nvarchar(128)
 AS
 BEGIN
     DECLARE @SQLString nvarchar(MAX)
 
     -- Construct the dynamic SQL statement
-    SET @SQLString = 'SELECT Id, Name FROM ' + QUOTENAME(@TableName)
+    SET @SQLString = 'SELECT Name FROM ' + QUOTENAME(@TableName)
 
     -- Execute the dynamic SQL
     EXEC sp_executesql @SQLString
