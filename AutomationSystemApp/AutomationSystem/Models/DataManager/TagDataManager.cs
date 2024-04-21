f25677db53d2d1594e75a7b5fe99d881793caa3b
MartinTande <mrtande95@gmail.com>
2024-04-21 15:22:33 +0200

    Add tag data manager

---
+++ AutomationSystemApp/AutomationSystem/Models/DataManager/TagDataManager.cs
@@ -0,0 +1,48 @@
+﻿using AutomationSystem.Models.DataAccess;
+
+namespace AutomationSystem.Models.DataManager;
+
+public class TagDataManager
+{
+    private readonly IDataConnector _sqlConnector;
+
+    public TagDataManager(IDataConnector sqlConnector)
+    {
+        _sqlConnector = sqlConnector;
+    }
+
+    public List<TagModel> GetTagsByObjectId(int objectId)
+    {
+        // Anonymous object, object with no name type
+        // Id is parameter of object, id is input to that parameter
+        var p = new { ObjectId = objectId };
+
+        List<TagModel> tagList = _sqlConnector.LoadData<TagModel, dynamic>("GetTagsByObjectId", p);
+        return tagList;
+    }
+
+    public void DeleteTag(int tagId)
+    {
+        var p = new { Id = tagId };
+
+        _sqlConnector.SaveData("DeleteTag", p);
+    }
+
+    public void InsertTag(TagModel tag)
+    {
+        // Anonymous object, object with no name type
+        var p = new
+        {
+            tag.Suffix,
+            tag.Description,
+            tag.SignalType,
+            tag.IOType,
+            tag.LowLimit,
+            tag.LowLowLimit,
+            tag.HighLimit,
+            tag.HighHighLimit
+        };
+
+        _sqlConnector.SaveData("CreateTag", p);
+    }
+}
