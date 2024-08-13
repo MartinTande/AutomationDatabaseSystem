dc545a1d32075acc157feb9ae6fa5e3985dbf60d
MartinTande <mrtande95@gmail.com>
2024-08-13 20:43:55 +0200

    ADD MUDBLAZOR TO PROJECT

--- AutomationListApp/AutomationListUI/Program.cs
+++ AutomationListApp/AutomationListUI/Program.cs
@@ -1,9 +1,9 @@
 using AutomationListLibrary.DataAccess;
 using AutomationListLibrary.DataManager;
-using AutomationListUI.Data;
 using AutomationListUI.Pages;
 using AutomationListUI.Readers;
 using AutomationListUI.Services;
+using MudBlazor.Services;
 using Syncfusion.Blazor;
 
 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
@@ -13,7 +13,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<ISqlConnector, SqlConnector>();
 builder.Services.AddSingleton<IObjectDataManager, ObjectDataManager>();
 builder.Services.AddSingleton<IObjectService, ObjectService>();
@@ -24,6 +23,7 @@ builder.Services.AddSingleton<TagDataManager>();
 builder.Services.AddSingleton<CustomerObjects.CustomAdaptor>();
 builder.Services.AddSingleton<ExcelReader>();
 builder.Services.AddSyncfusionBlazor();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 //Syncfusion license
