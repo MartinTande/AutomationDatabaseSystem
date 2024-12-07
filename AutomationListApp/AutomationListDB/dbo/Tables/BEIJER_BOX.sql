deae7efac4dc42809c30d90d31c7eec5803c8e96
Martin Tande <mrtande95@gmail.com>
2024-12-07 15:12:16 +0100

    squash

--- AutomationListApp/AutomationListDB/dbo/Tables/BEIJER_BOX.sql
+++ AutomationListApp/AutomationListDB/dbo/Tables/BEIJER_BOX.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[BEIJER_BOX] (
     [Id]        INT           PRIMARY KEY IDENTITY (1, 1) NOT NULL,
     [Name]      VARCHAR (200) NULL,
-    [IpAddress] VARCHAR (100) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    [IpAddress] VARCHAR (100) NULL
 );
 
--- AutomationListApp/AutomationListDB/dbo/Tables/SIGNAL_TYPE.sql
+++ AutomationListApp/AutomationListDB/dbo/Tables/SIGNAL_TYPE.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[SIGNAL_TYPE] (
     [Id]   INT          PRIMARY KEY IDENTITY (1, 1) NOT NULL,
-    [Name] VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Name] VARCHAR (50) NULL
 );
 
